10fddcee8b43ac92aa1f84a704fa30a624112ffe
Hajar <hajar1445@gmail.com>
2023-05-20 17:42:26 -0700

    adding header file

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+#endif
